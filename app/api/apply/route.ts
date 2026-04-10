c176887f798e5e2bf4cffd24a7febe6e5f83c01c
Mastermindddddd <malusicephas17@gmail.com>
2026-04-10 04:48:07 +0200

    changed AWS environment variables

--- app/api/apply/route.ts
+++ app/api/apply/route.ts
@@ -5,10 +5,10 @@ import Application from "@/models/Application";
 import { verifyToken } from "@/lib/auth";
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION!,
+  region: process.env.MY_AWS_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY!,
   },
 });
 
